e48aa94491e33e715444e65774c59ed2a87c25c8
Shilpika S <sshilpika5@gmail.com>
2016-06-29 16:32:54 -0500

    converting text to parquet

--- src/main/scala/MemTimeCheck.scala
+++ src/main/scala/MemTimeCheck.scala
@@ -5,74 +5,53 @@ import org.apache.spark.{SparkConf, SparkContext}
   */
 object MemTimeCheck {
 
-  case class Config(dir: Option[String] = None, ext: Option[String] = None,
-                    slices: Int = 1, executor: String = "1g", fileN: String = "",
-    maxc:Int= 1, ecore:Int = 1)
 
-
-  def parseCommandLine(args: Array[String]): Option[Config] = {
-    val parser = new scopt.OptionParser[Config]("scopt") {
-      head("LineCount", "1.0")
-      opt[String]('d', "dir") action { (x, c) =>
-        c.copy(dir = Some(x))
-      } text ("dir is a String property")
-      opt[Int]('c', "ecore") action { (x, c) =>
-        c.copy(ecore = x)
-      } text ("dir is a String property")
-      opt[Int]('m', "maxc") action { (x, c) =>
-        c.copy(maxc = x)
-      } text ("max core is a Int property")
-      opt[String]('e', "extension") action { (x, c) =>
-        c.copy(ext = Some(x))
-      } text ("ext is a String property")
-      opt[Int]('s', "slices") action { (x, c) =>
-        c.copy(slices = x)
-      } text ("slices is an Int property")
-      opt[String]('x', "executor") action { (x, c) =>
-        c.copy(executor = x)
-      } text ("executor is an Int property")
-      opt[String]('f', "fileN") action { (x, c) =>
-        c.copy(fileN = x)
-      } text ("FileName is a String property")
-      help("help") text ("prints this usage text")
-
-    }
-    // parser.parse returns Option[C]
-    parser.parse(args, Config())
-  }
 
   def main(args: Array[String]): Unit={
 
-    val appConfig = parseCommandLine(args).getOrElse(Config())
-    val partitions = appConfig.slices
-    val exec = appConfig.executor
-    val maxc = appConfig.maxc
-    val ecore = appConfig.ecore
 
-    val conf = new SparkConf().setAppName("Spark-Performance-Cooley").
-      set("spark.cores.max",maxc.toString).
-      set("spark.executor.memory",exec).
-      set("spark.executor.cores",ecore.toString)//.
-      //set("","")
-    val spark = new SparkContext(conf)
-    val sqlContext = new org.apache.spark.sql.SQLContext(spark)
+    val conf = new SparkConf().setAppName("Create-Parquet")
+
+    val sc = new SparkContext(conf)
+
+    // sc is an existing SparkContext.
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
     import sqlContext.implicits._
+    // Create an RDD
+    val number = sc.textFile("/projects/ExaHDF5/sshilpika/bf2.txt")
+
+    // The schema is encoded in a string
+    val schemaString = "num"
+
+    // Import Row.
+    import org.apache.spark.sql.Row
+
+    // Import Spark SQL data types
+    import org.apache.spark.sql.types.{StructType,StructField,StringType};
+
+    // Generate the schema based on the string of schema
+    val schema =
+      StructType(
+        schemaString.split(" ").map(fieldName => StructField(fieldName, StringType, true)))
+
+    // Convert records of the RDD (people) to Rows.
+    val rowRDD = number.map(p => Row(p.trim))
+
+    // Apply the schema to the RDD.
+    val numberDataFrame = sqlContext.createDataFrame(rowRDD, schema)
+
+    // Register the DataFrames as a table.
+    numberDataFrame.registerTempTable("numbers")
+
+    // SQL statements can be run by using the sql methods provided by sqlContext.
+    val results = sqlContext.sql("SELECT num FROM numbers")
+
+    // The results of SQL queries are DataFrames and support all the normal RDD operations.
+    // The columns of a row in the result can be accessed by field index or by field name.
+    results.map(t => "Number: " + t(0)).collect().foreach(println)
 
 
-//    val path = appConfig.dir.getOrElse("./data")
-//    val extension = appConfig.ext.getOrElse(".txt")
-
-//    val fileN = appConfig.fileN
-
-    //val rdd = spark.parallelize(1 to 100000000, 3)
-    val rdd = spark.textFile("/projects/ExaHDF5/sshilpika/bF.txt",partitions)
-    //val res1 = rdd.reduce(_+_)
-    val result = rdd.map(_ + 5).count()
-
-    println(s"The result is $result the default cores are ${spark.defaultParallelism} and partitions used  are ${rdd.partitions.length}")
-    println(s"The debug string is ${rdd.toDebugString}")
-    //println(s"The sum is ${res1}")
-    //spark.stop()
+    results.write.parquet("/projects/ExaHDF5/sshilpika/bf.parquet")
   }
 
 
