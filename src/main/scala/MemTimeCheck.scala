a2f258850920c5f7f4109e52d787cd1a8ac27d26
Shilpika S <sshilpika5@gmail.com>
2016-07-18 21:36:04 -0500

    adding 1.85TB

--- src/main/scala/MemTimeCheck.scala
+++ src/main/scala/MemTimeCheck.scala
@@ -62,7 +62,7 @@ object MemTimeCheck {
 
 
     val rdd = spark.textFile("/projects/ExaHDF5/sshilpika/BIGD",partitions)
-    val df = rdd.toDF("num")
+    val df = sqlContext.read.text("/projects/ExaHDF5/sshilpika/BIGD")//rdd.toDF("num")
     val ds = sqlContext.read.text("/projects/ExaHDF5/sshilpika/BIGD").as[String]
 
     //RDD
